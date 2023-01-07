f03f91acb865aad110f0e7c126d224766e39d716
Octavio Lugo Camacho <octaviolugocamacho@hotmail.com>
2023-01-07 12:51:20 -0600

    Add features

--- Gemfile
+++ Gemfile
@@ -9,3 +9,5 @@ gem "rspec", "~> 3.12"
 gem "cucumber", "~> 8.0"
 
 gem "selenium-cucumber", "~> 3.1"
+
+gem "sinatra", "~> 3.0"
---
+++ features/step_definitions/code_breaker_steps.rb
@@ -0,0 +1,20 @@
+When('I go to the app') do
+    visit "localhost:4567"
+  end
+  
+  Then('I should see {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given('I have entered {int} fopr guess') do |int|
+  # Given('I have entered {float} fopr guess') do |float|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I press {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('the result should be {string} on the screen') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
